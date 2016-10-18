9e0ec2c0908be52a76656893fa63ee87927e9f11
mahrye1116 <mariah.azoti@gmail.com>
2016-10-17 21:52:01 -0400

    added new pages

--- server.rb
+++ server.rb
@@ -11,3 +11,19 @@ end
 get '/about' do
   erb :about
 end
+
+get '/appraisals' do
+  erb :appraisals
+end
+
+get '/photography' do
+  erb :photography
+end
+
+get '/links' do
+  erb :links
+end
+
+get '/contact' do
+  erb :contact
+end
