e7d8d0b533726eaa29e67c3992bf6cffd5a6507f
Nathan Wright <nate01776@gmail.com>
2021-02-13 18:14:39 -0500

    placement and color working across desktop and mobile

--- src/App.js
+++ src/App.js
@@ -4,19 +4,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="main-title">
+        
+        <div className="App-header">
           <h1>Mariah Azoti</h1>
           <h2>Digital Marketing & Art Consulting Services</h2>
-          <h3>
-            Demonstrated history in social media strategy and implementation, 
-            digital asset management, brand development, and more.
-          </h3>
         </div>
-      </header>
-      <div className="App-body">
-        
+
+        <div className="App-body">
+            <div className="service-name">
+              social media strategy
+            </div>
+            <div className="service-name">
+              •
+            </div>
+            <div className="service-name">
+               digital asset management
+            </div>
+            <div className="service-name">
+              •
+            </div>
+            <div className="service-name">
+              brand development
+            </div>
+            <div className="service-name">
+              •
+            </div>
+            <div className="service-name">
+              art logistics
+            </div>
+        </div>
+
+      <div className="App-footer">
+          <a href="http://www.linkedin.com/in/mariahazoti" target="_blank">
+            <div className="contact-link">
+              LINKEDIN >>
+            </div>
+          </a>
+          <a href="http://www.instagram.com/mariahazoti" target="_blank">
+            <div className="contact-link bottom">
+              INSTAGRAM >>
+            </div>
+          </a>
       </div>
+
     </div>
   );
 }
